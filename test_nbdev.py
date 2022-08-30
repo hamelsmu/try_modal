89f7fed404b8794ce5ede62a139b8aead7bc4c5e
Hamel Husain <hamel.husain@gmail.com>
2022-08-30 10:39:16 -0700

    save

--- test_nbdev.py
+++ test_nbdev.py
@@ -5,12 +5,10 @@ stub = modal.Stub("ci")
 image_with_model = (
     modal.DebianSlim()
     .apt_install(["git"])
-    .pip_install(["git+https://github.com/fastai/fastcore.git",
-                 "git+https://github.com/fastai/execnb.git"])  
+    .pip_install(["git+https://github.com/fastai/fastcore.git", "git+https://github.com/fastai/execnb.git"])  
 )
 
-@stub.function(image=image_with_model,
-               mounts=[modal.Mount(local_dir="./nbdev", remote_dir="/root/nbdev")])
+@stub.function(image=image_with_model, mounts=[modal.Mount(local_dir="./nbdev", remote_dir="/root/nbdev")])
 def ci():
     os.chdir('nbdev')
     subprocess.call(["pip", "install", "-q", ".[dev]"])
